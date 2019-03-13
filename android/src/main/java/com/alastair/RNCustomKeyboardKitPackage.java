7ff05a35a62192a314c013afd25c525c905ec2fa
Alastair Smith <25154859+Alastair-smith2@users.noreply.github.com>
2019-03-13 10:44:42 +0000

    Resolve name spacing

---
+++ android/src/main/java/com/alastair/RNCustomKeyboardKitPackage.java
@@ -0,0 +1,29 @@
+
+package com.alastair;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import com.facebook.react.ReactPackage;
+import com.facebook.react.bridge.NativeModule;
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.uimanager.ViewManager;
+import com.facebook.react.bridge.JavaScriptModule;
+import com.facebook.react.uimanager.RNCustomKeyboardKitModule;
+public class RNCustomKeyboardKitPackage implements ReactPackage {
+  @Override
+  public List<NativeModule> createNativeModules(ReactApplicationContext reactContext) {
+    return Arrays.<NativeModule>asList(new RNCustomKeyboardKitModule(reactContext));
+  }
+
+  @Override
+  public List<Class<? extends JavaScriptModule>> createJSModules() {
+    return Collections.emptyList();
+  }
+
+  @Override
+  public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
+    return Collections.emptyList();
+  }
+}
